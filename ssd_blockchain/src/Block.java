8bc5d7b50cf171434b0944ae3a02a2193b44bfb4
Henrique Barreira <henrique.barreira@portodigital.pt>
2024-05-11 18:49:08 +0100

    Block has verify Block and new mine function

--- ssd_blockchain/src/Block.java
+++ ssd_blockchain/src/Block.java
@@ -1,57 +1,51 @@
 import java.util.List;
 
 public class Block {
+    private String blockHash;
     private String prevHash;
     private long timestamp;
-    private int nonce;
+    private int nonce=0;
     private String merkleRoot;
-    private String blockHash;
     private List<Transaction> transactions;
-    private int difficulty;
+    private String publicKey;
 
-    public Block(String prev_hash, List<Transaction> transactions, int difficulty) {
+    public Block(String prev_hash, List<Transaction> transactions) {
         this.prevHash = prev_hash;
         this.transactions = transactions;
         this.merkleRoot = calculateMerkleRoot(transactions);
         this.timestamp = System.currentTimeMillis();
-        this.difficulty = difficulty;
-        mineBlock();
+        this.blockHash = mineBlock();
     }
 
     public String getPrevHash() {
         return prevHash;
     }
 
-    public String getTransactions() {
-        return transactions.toString();
+    public String getBlockHash(){
+        return blockHash;
     }
 
-    @Override
-    public String toString() {
-        return "Block[" +
-                "prev_hash=" + prevHash +
-                ", timestamp=" + timestamp +
-                ", nonce=" + timestamp +
-                ", merkle_root=" + merkleRoot +
-                ", hash=" + blockHash +
-                ']';
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public int getNumberOfTransactions(){
+        return transactions.size();
     }
 
     private String calculateMerkleRoot(List<Transaction> transactions ) {
         MerkleTree merkleTree = new MerkleTree(transactions);
-        System.out.println(merkleTree);
         return merkleTree.getMerkleRoot();
     }
 
-    private void mineBlock() {
-        // Start with nonce value of 0
-        nonce = 0;
+    private String mineBlock() {
         // Iterate until a valid hash is found
         while (!isValidHash(calculateHash())) {
             nonce++;
         }
         // Valid hash found, block mined
         blockHash = calculateHash();
+        return blockHash;
     }
 
     private String calculateHash() {
@@ -62,8 +56,22 @@ public class Block {
 
     private boolean isValidHash(String hash) {
         // Check if the hash has the required number of leading zeros
-        String prefix = "0".repeat(difficulty);
+        String prefix = "0".repeat(Config.DIFFICULTY);
         return hash.startsWith(prefix);
     }
 
+    public boolean verifyBlock(){
+        return calculateHash().equals(blockHash);
+    }
+
+    @Override
+    public String toString() {
+        return "Block[" +
+                "prev_hash=" + prevHash +
+                ", timestamp=" + timestamp +
+                ", nonce=" + nonce +
+                ", merkle_root=" + merkleRoot +
+                ", hash=" + blockHash +
+                ']';
+    }
 }
