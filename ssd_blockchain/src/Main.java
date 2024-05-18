ba20161f5ebe54e07c045e1767c9c7ffb884eaba
Henrique Barreira <henrique.barreira@portodigital.pt>
2024-05-18 18:37:11 +0100

    tested tampering transactions for 2nd block

--- ssd_blockchain/src/Main.java
+++ ssd_blockchain/src/Main.java
@@ -27,15 +27,20 @@ public class Main {
         System.out.println("blockchain final: " + chain);
 
         System.out.println("blockchain valid: " + chain.isValid());
+        System.out.println("-----Alteração do Bloco 1-------");
+        System.out.println("Block1 Transactions" + chain.getBlockchain().get(1).getTransactions());
 
-        //Transaction transaction1_alt = new Transaction(Transaction.TransactionType.BID, "Joao deu bid $50");
-        //Transaction transaction2_alt = new Transaction(Transaction.TransactionType.BID, "Julio deu bid $60");
-        //List<Transaction> transactions_alt = new ArrayList<>();
-        //transactions_alt.add(transaction1_alt);
-        //transactions_alt.add(transaction2_alt);
-        //chain.getBlockchain().get(1).setTransactions(transactions_alt);
+        //Criar lista de transacoes alterada
+        Transaction transaction1_alt = new Transaction(Transaction.TransactionType.BID, "Joao deu bid $50");
+        Transaction transaction2_alt = new Transaction(Transaction.TransactionType.BID, "Julio deu bid $60");
+        List<Transaction> transactions_alt = new ArrayList<>();
+        transactions_alt.add(transaction1_alt);
+        transactions_alt.add(transaction2_alt);
 
-        //System.out.println("blockchain valid: " + chain.isValid());
+        // Modificar transacoes do bloco1
+        chain.getBlockchain().get(1).setTransactions(transactions_alt);
+        System.out.println("Block1 Transactions depois de alteracao" + chain.getBlockchain().get(1).getTransactions());
+        System.out.println("blockchain valid: " + chain.isValid());
 
     }
 
