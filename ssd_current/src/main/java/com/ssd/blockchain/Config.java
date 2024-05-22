af5f82cea5e804f052eca57690b4075b6e98295b
henrique <henrique.barreira@portodigital.pt>
2024-05-22 21:29:51 +0100

    sha256 set on config file

--- ssd_current/src/main/java/com/ssd/blockchain/Config.java
+++ ssd_current/src/main/java/com/ssd/blockchain/Config.java
@@ -2,4 +2,5 @@ package com.ssd.blockchain;
 public class Config {
     public static int DIFFICULTY = 4; // Set the default difficulty here
     public static int MAX_TRANSACTIONS= 4;
+    public static String HASH_ALG = "SHA-256";
 }
